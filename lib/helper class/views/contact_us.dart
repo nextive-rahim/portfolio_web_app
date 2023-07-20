5670ac8b69237ea3cd9800ae2989244c3dcc19de
Abdul Rahim <113752743+nextive-rahim@users.noreply.github.com>
2023-07-21 01:05:48 +0600

    fixed : Update contact us page design.

--- lib/helper class/views/contact_us.dart
+++ lib/helper class/views/contact_us.dart
@@ -1,11 +1,10 @@
-
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dev_portfolio/globals/app_buttons.dart';
 import 'package:flutter_dev_portfolio/globals/app_colors.dart';
 import 'package:flutter_dev_portfolio/globals/app_text_styles.dart';
 import 'package:flutter_dev_portfolio/globals/constants.dart';
 import 'package:flutter_dev_portfolio/helper%20class/helper_class.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactUs extends StatelessWidget {
   const ContactUs({Key? key}) : super(key: key);
@@ -24,10 +23,9 @@ class ContactUs extends StatelessWidget {
             borderRadius: BorderRadius.circular(10),
             color: Colors.transparent,
             elevation: 8,
-            child: TextField(
-              cursorColor: AppColors.white,
-              style: AppTextStyles.normalStyle(),
-              decoration: buildInputDecoration(hintText: 'Full Name'),
+            child: buildInputDecoration(
+              title: 'Name : ',
+              label: 'Abdul Rahim',
             ),
           ),
           Constants.sizedBox(height: 20.0),
@@ -35,10 +33,13 @@ class ContactUs extends StatelessWidget {
             borderRadius: BorderRadius.circular(10),
             color: Colors.transparent,
             elevation: 8,
-            child: TextField(
-              cursorColor: AppColors.white,
-              style: AppTextStyles.normalStyle(),
-              decoration: buildInputDecoration(hintText: 'Email Address'),
+            child: buildInputDecoration(
+              title: 'Email : ',
+              label: 'rahimsr983@gmail.com',
+              onTap: () {
+                emailSupport();
+              },
+              isShowUnderline: true,
             ),
           ),
           Constants.sizedBox(height: 20.0),
@@ -46,39 +47,28 @@ class ContactUs extends StatelessWidget {
             borderRadius: BorderRadius.circular(10),
             color: Colors.transparent,
             elevation: 8,
-            child: TextField(
-              cursorColor: AppColors.white,
-              style: AppTextStyles.normalStyle(),
-              decoration: buildInputDecoration(hintText: 'Mobile Number'),
+            child: buildInputDecoration(
+              title: 'phone : ',
+              label: '01718663032',
+            ),
+          ),
+          Constants.sizedBox(width: 20.0),
+          Expanded(
+            child: Material(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.transparent,
+              elevation: 8,
+              child: buildInputDecoration(
+                title: 'Whatsapp : ',
+                label: '01718663032',
+                onTap: () {
+                  whatappSupport();
+                },
+                isShowUnderline: true,
+              ),
             ),
           ),
           Constants.sizedBox(height: 20.0),
-          Material(
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.transparent,
-            elevation: 8,
-            child: TextField(
-              cursorColor: AppColors.white,
-              style: AppTextStyles.normalStyle(),
-              decoration: buildInputDecoration(hintText: 'Email Subject'),
-            ),
-          ),
-          Constants.sizedBox(height: 20.0),
-          Material(
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.transparent,
-            elevation: 8,
-            child: TextField(
-              maxLines: 15,
-              cursorColor: AppColors.white,
-              style: AppTextStyles.normalStyle(),
-              decoration: buildInputDecoration(hintText: 'Your Message'),
-            ),
-          ),
-          Constants.sizedBox(height: 40.0),
-          AppButtons.buildMaterialButton(
-              buttonName: 'Send Message', onTap: () {}),
-          Constants.sizedBox(height: 30.0),
         ],
       ),
       tablet: buildForm(),
@@ -102,10 +92,9 @@ class ContactUs extends StatelessWidget {
                 borderRadius: BorderRadius.circular(10),
                 color: Colors.transparent,
                 elevation: 8,
-                child: TextField(
-                  cursorColor: AppColors.white,
-                  style: AppTextStyles.normalStyle(),
-                  decoration: buildInputDecoration(hintText: 'Full Name'),
+                child: buildInputDecoration(
+                  title: 'Name : ',
+                  label: 'Abdul Rahim',
                 ),
               ),
             ),
@@ -115,10 +104,13 @@ class ContactUs extends StatelessWidget {
                 borderRadius: BorderRadius.circular(10),
                 color: Colors.transparent,
                 elevation: 8,
-                child: TextField(
-                  cursorColor: AppColors.white,
-                  style: AppTextStyles.normalStyle(),
-                  decoration: buildInputDecoration(hintText: 'Email Address'),
+                child: buildInputDecoration(
+                  title: 'Email : ',
+                  label: 'rahimsr983@gmail.com',
+                  onTap: () {
+                    emailSupport();
+                  },
+                  isShowUnderline: true,
                 ),
               ),
             ),
@@ -132,10 +124,9 @@ class ContactUs extends StatelessWidget {
                 borderRadius: BorderRadius.circular(10),
                 color: Colors.transparent,
                 elevation: 8,
-                child: TextField(
-                  cursorColor: AppColors.white,
-                  style: AppTextStyles.normalStyle(),
-                  decoration: buildInputDecoration(hintText: 'Mobile Number'),
+                child: buildInputDecoration(
+                  title: 'Phone : ',
+                  label: '01718663032',
                 ),
               ),
             ),
@@ -145,10 +136,13 @@ class ContactUs extends StatelessWidget {
                 borderRadius: BorderRadius.circular(10),
                 color: Colors.transparent,
                 elevation: 8,
-                child: TextField(
-                  cursorColor: AppColors.white,
-                  style: AppTextStyles.normalStyle(),
-                  decoration: buildInputDecoration(hintText: 'Email Subject'),
+                child: buildInputDecoration(
+                  title: 'Whatsapp : ',
+                  label: '01718663032',
+                  onTap: () {
+                    whatappSupport();
+                  },
+                  isShowUnderline: true,
                 ),
               ),
             ),
@@ -159,17 +153,15 @@ class ContactUs extends StatelessWidget {
           borderRadius: BorderRadius.circular(10),
           color: Colors.transparent,
           elevation: 8,
-          child: TextField(
-            maxLines: 15,
-            cursorColor: AppColors.white,
-            style: AppTextStyles.normalStyle(),
-            decoration: buildInputDecoration(hintText: 'Your Message'),
+          child: buildInputDecoration(
+            title: 'Messanger : ',
+            label: 'https://m.me/rahim.srrahim.3',
+            onTap: () {
+              _launchUrl('https://m.me/rahim.srrahim.3');
+            },
+            isShowUnderline: true,
           ),
         ),
-        Constants.sizedBox(height: 40.0),
-        AppButtons.buildMaterialButton(
-            buttonName: 'Send Message', onTap: () {}),
-        Constants.sizedBox(height: 30.0),
       ],
     );
   }
@@ -193,19 +185,88 @@ class ContactUs extends StatelessWidget {
     );
   }
 
-  InputDecoration buildInputDecoration({required String hintText}) {
-    return InputDecoration(
-        hintText: hintText,
-        hintStyle: AppTextStyles.comfortaaStyle(),
-        enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide.none),
-        focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide.none),
-        filled: true,
-        fillColor: AppColors.bgColor2,
-        contentPadding:
-            const EdgeInsets.symmetric(horizontal: 24, vertical: 16));
+  Widget buildInputDecoration({
+    required String label,
+    required String title,
+    void Function()? onTap,
+    bool isShowUnderline = false,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 16,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: AppColors.bgColor2,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 16,
+                color: Colors.white,
+                letterSpacing: 1.7,
+                height: 1.5,
+              ),
+            ),
+            Text(
+              label,
+              style: TextStyle(
+                shadows: isShowUnderline
+                    ? const [
+                        Shadow(
+                          color: Colors.white,
+                          offset: Offset(0, -3),
+                        )
+                      ]
+                    : null,
+                color: isShowUnderline ? Colors.transparent : Colors.white,
+                decoration: isShowUnderline ? TextDecoration.underline : null,
+                decorationColor: Colors.white,
+                decorationThickness: 2,
+                fontWeight: FontWeight.w500,
+                fontSize: 16,
+                letterSpacing: 1.7,
+                height: 1.5,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Email Support
+  Future<void> emailSupport() async {
+    final Uri launchUri = Uri(
+      scheme: 'mailto',
+      path: 'rahimsr983@gmail.com',
+    );
+    await _launchUrl(
+      launchUri.toString(),
+    );
+  }
+
+// Hotline Support
+  Future<void> whatappSupport() async {
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: '+8801718 663 032',
+    );
+    await _launchUrl(
+      launchUri.toString(),
+    );
+  }
+
+  Future<void> _launchUrl(String socialLink) async {
+    if (!await launchUrl(Uri.parse(socialLink))) {
+      throw Exception('Could not launch $socialLink');
+    }
   }
 }
